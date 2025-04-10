ef35b8f454950f8ebebd0a6013f23c987b0a33e2
BlesonPhilip <blesonphilip21@gmail.com>
2025-04-10 16:33:40 +0530

    get by id and delete

--- server.js
+++ server.js
@@ -26,6 +26,27 @@ app.post("/book", async (req, res) => {
   }
 });
 
+app.get("/book/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findById(id);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+    return res.status(200).json(book);
+  } catch (error) {
+    return res.status(500).json({ message: err.message });
+  }
+});
+
+app.delete("/book/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = await Book.findByIdAndDelete(id);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+    return res.status(200).json({ message: "Book deleted successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: err.message });
+  }
+});
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
